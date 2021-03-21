52698409f5d6bb71d28ca3530de5eb3dedde1a43
Ali Mustafa <ali.00622274@gmail.com>
2021-03-21 22:22:15 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -12,8 +12,8 @@ export default class EyesOnMe extends React.Component {
   }
   render() {
     return (
-      <button onFocus={this.onFocus} onBlur={this.onBlur}/>
-       { Eyes on me, please!
+      <button onFocus={this.onFocus} onBlur={this.onBlur}>
+        Eyes on me, please!
       </button>
     )
   }
