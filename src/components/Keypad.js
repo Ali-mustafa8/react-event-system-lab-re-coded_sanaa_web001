3dea1d1ed97a6f93aa580680818938ac568aa5f5
Ali Mustafa <ali.00622274@gmail.com>
2021-03-21 22:17:38 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,11 +2,12 @@ import React from "React";
 export default class kypad extends React.Component{
   
   const keyUp=()=>{
+    console.log("Entering password");
     
   }
   render(){
     return(
-      <input>
+      <input type="password">
       
       )
     
