6ad9701ab8115fcff663717d8e319fe43fe4ebb5
Ali Mustafa <ali.00622274@gmail.com>
2021-03-21 22:21:05 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,11 +5,12 @@ export default class kypad extends React.Component{
     console.log("Entering password");
     
   }
+  
   render(){
     return(
       <input type="password" onKeyUp={this.keyevent}>
       
-      )
+      );
     
   }
 }
